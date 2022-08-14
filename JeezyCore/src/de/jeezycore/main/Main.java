3eb9f26804caefd2e3e08a7da9885bafc73b4c41
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-08-14 02:49:28 +0200

    push JeezyCore.

---
+++ JeezyCore/src/de/jeezycore/main/Main.java
@@ -0,0 +1,13 @@
+package de.jeezycore.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Success!");
+    }
+
+
+}
