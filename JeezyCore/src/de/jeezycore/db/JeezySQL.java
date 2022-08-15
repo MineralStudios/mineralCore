33a12671da952c82043ad3adb42276a6cca916c2
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-08-16 00:14:20 +0200

    Recoded JeezySQl created separated methods createTable()/pushData().

--- JeezyCore/src/de/jeezycore/db/JeezySQL.java
+++ JeezyCore/src/de/jeezycore/db/JeezySQL.java
@@ -4,25 +4,58 @@ import de.jeezycore.colors.Color;
 import java.sql.*;
 
 public class JeezySQL  {
-    public void createConnection() {
+public String url;
+public String user;
+public String password;
 
-        String url = "jdbc:mysql://localhost:3306/jeezydevelopment";
-        String user = "root";
-        String password = "";
+    private void createConnection() {
+
+        url = "jdbc:mysql://localhost:3306/jeezydevelopment";
+        user = "root";
+        password = "";
         try {
             Connection con = DriverManager.getConnection(url, user, password);
             System.out.println(Color.WHITE_BOLD+"[JeezyDevelopment] "+Color.GREEN_BOLD+"Successfully"+Color.CYAN+" connected to database."+Color.RESET);
-
-            Statement stm = con.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS JeezyCore " +
-                    " (rankName VARCHAR(255), " +
-                    " rankFormat VARCHAR(255), " +
-                    " PRIMARY KEY ( rankName ))";
-            stm.executeUpdate(sql);
             System.out.println(Color.WHITE_BOLD+"[JeezyDevelopment] "+Color.GREEN_BOLD+"Successfully"+Color.CYAN+" created"+Color.YELLOW_BOLD+" jeezyCore"+Color.CYAN+" table."+Color.RESET);
         } catch (SQLException e) {
             System.out.println(Color.WHITE_BOLD+"[JeezyDevelopment]"+Color.RED_BOLD+" Something went wrong when tried to connect to your database."+Color.RESET);
             System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void createTable() {
+        try {
+            this.createConnection();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS JeezyCore " +
+                    " (rankName VARCHAR(255), " +
+                    " rankColor VARCHAR(255), " +
+                    " playerName VARCHAR(255), " +
+                    " PRIMARY KEY ( rankName ))";
+            stm.executeUpdate(sql);
+        } catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+
+    public void pushData(String sql, String rankName, String rankColor, String playerName)  {
+    this.createConnection();
+        try {
+            System.out.println(sql);
+            Connection con = DriverManager.getConnection(url, user, password);
+
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, rankName);
+            pstmt.setString(2, rankColor);
+            pstmt.setString(3, playerName);
+
+            pstmt.executeUpdate();
+
+
+        } catch (SQLException e) {
+            System.out.println(e);
         }
 
     }
