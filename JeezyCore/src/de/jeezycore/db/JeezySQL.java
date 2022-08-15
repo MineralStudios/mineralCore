61aaf1e7e22cea6f181852c42c89aa171758a7fe
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-08-15 12:21:56 +0200

    Created Table in JeezySQl on startup.

--- JeezyCore/src/de/jeezycore/db/JeezySQL.java
+++ JeezyCore/src/de/jeezycore/db/JeezySQL.java
@@ -1,9 +1,8 @@
 package de.jeezycore.db;
 import de.jeezycore.colors.Color;
 // SQL imports
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
 public class JeezySQL  {
     public void createConnection() {
 
@@ -14,10 +13,18 @@ public class JeezySQL  {
             Connection con = DriverManager.getConnection(url, user, password);
             System.out.println(Color.WHITE_BOLD+"[JeezyDevelopment] "+Color.GREEN_BOLD+"Successfully"+Color.CYAN+" connected to database."+Color.RESET);
 
+            Statement stm = con.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS JeezyCore " +
+                    " (rankName VARCHAR(255), " +
+                    " rankFormat VARCHAR(255), " +
+                    " PRIMARY KEY ( rankName ))";
+            stm.executeUpdate(sql);
+            System.out.println(Color.WHITE_BOLD+"[JeezyDevelopment] "+Color.GREEN_BOLD+"Successfully"+Color.CYAN+" created"+Color.YELLOW_BOLD+" jeezyCore"+Color.CYAN+" table."+Color.RESET);
         } catch (SQLException e) {
             System.out.println(Color.WHITE_BOLD+"[JeezyDevelopment]"+Color.RED_BOLD+" Something went wrong when tried to connect to your database."+Color.RESET);
-            //System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
         }
+
     }
 
 }
