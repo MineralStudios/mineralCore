89bf026e9489ac318b377d1c50b9ffd601985a2e
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-08-18 00:52:39 +0200

    Tried to display array data(not working yet).

--- JeezyCore/src/de/jeezycore/commands/GrantRank.java
+++ JeezyCore/src/de/jeezycore/commands/GrantRank.java
@@ -1,5 +1,6 @@
 package de.jeezycore.commands;
 
+import de.jeezycore.db.JeezySQL;
 import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,6 +11,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.material.Wool;
+
+import java.util.ArrayList;
 
 public class GrantRank implements CommandExecutor {
     @Override
@@ -23,19 +26,35 @@ public class GrantRank implements CommandExecutor {
             } else {
                 p.sendMessage("You want to grant a rank to the player: "+args[0]);
                 Inventory inv = Bukkit.createInventory(null, 27, ChatColor.AQUA + "Grant " +ChatColor.WHITE+ " §8§l[ "+ChatColor.WHITE+args[0]+" §8§l]");
-                Wool wool = new Wool(DyeColor.RED);
-                ItemStack rank = new ItemStack(wool.toItemStack(1));
-                ItemMeta rankMeta = rank.getItemMeta();
-                rankMeta.setDisplayName("§cOwner");
+
                 ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
                 SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
                 skullMeta.setDisplayName("§b"+args[0]);
                 skullMeta.setOwner(args[0]);
-                rank.setItemMeta(rankMeta);
+
                 skull.setItemMeta(skullMeta);
                 inv.setItem(4, skull);
-                inv.setItem(9, rank);
-                p.openInventory(inv);
+                JeezySQL display = new JeezySQL();
+                display.displayData();
+
+                System.out.println(display.rankData);
+                if (display.rankData.size() == 0) {
+                    p.openInventory(inv);
+                    return true;
+                } else {
+                    for (String i : display.rankData) {
+                        for (int f = 0; f < display.rankData.size(); f++) {
+                            Wool wool = new Wool(DyeColor.RED);
+                            ItemStack rank = new ItemStack(wool.toItemStack(1));
+                            ItemMeta rankMeta = rank.getItemMeta();
+                            rankMeta.setDisplayName(i);
+                            rank.setItemMeta(rankMeta);
+                            inv.setItem(f, rank);
+                        }
+                    }
+                    p.openInventory(inv);
+                }
+
             }
 
         }
