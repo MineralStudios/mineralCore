ed4cac8ac287898bc2f0a38628830954ad752b50
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-08-23 22:50:36 +0200

    Got ChatEvent working with the rank system dynamically updating (simple).

--- JeezyCore/src/de/jeezycore/events/ChatEvent.java
+++ JeezyCore/src/de/jeezycore/events/ChatEvent.java
@@ -1,5 +1,6 @@
 package de.jeezycore.events;
 
+import de.jeezycore.colors.ColorTranslator;
 import de.jeezycore.db.JeezySQL;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,11 +11,17 @@ public class ChatEvent implements Listener {
     @EventHandler
     public void onPlayerChat1(AsyncPlayerChatEvent e) {
         JeezySQL display = new JeezySQL();
-        display.displayData();
-        if (display.player == null) return;
-        if (display.player.equalsIgnoreCase(e.getPlayer().getName())) {
-            e.setFormat("§7§l["+display.rankColor+""+display.rank+"§7§l]§f "+e.getPlayer().getDisplayName()+": "+e.getMessage());
-        }
+        String sql = "SELECT * FROM jeezycore WHERE playerName LIKE '%"+e.getPlayer().getDisplayName()+"%'";
+        display.displayChatRank(sql);
+        System.out.println(display.rank);
+        System.out.println(display.rankColor);
+
+            if (display.rank == null) return;
+
+            String show_color = ColorTranslator.colorTranslator.get(display.rankColor);
+            System.out.println(show_color);
+            e.setFormat("§7§l["+show_color+""+display.rank+"§7§l]§f "+e.getPlayer().getDisplayName()+": "+e.getMessage());
+
 
     }
 
