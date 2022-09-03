7510263895219b9ca7ec6495112fe9544d7c5fee
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-09-04 01:34:58 +0200

    Implemented json config read and also handle it in ChatEvent.

--- JeezyCore/src/de/jeezycore/events/ChatEvent.java
+++ JeezyCore/src/de/jeezycore/events/ChatEvent.java
@@ -2,11 +2,13 @@ package de.jeezycore.events;
 
 import de.jeezycore.colors.ColorTranslator;
 import de.jeezycore.db.JeezySQL;
-import de.jeezycore.utils.UUIDChecker;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
 
 public class ChatEvent implements Listener {
 
@@ -20,6 +22,28 @@ public class ChatEvent implements Listener {
         System.out.println(e.getPlayer().getUniqueId());
 
             if (display.rank == null) return;
+
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("C:\\Users\\Lassd\\IdeaProjects\\JeezyDevelopment\\JeezyCore\\src\\config.json"));
+            // A JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
+            JSONObject jsonObject = (JSONObject) obj;
+            // A JSON array. JSONObject supports java.util.List interface.
+            Boolean chat_muted = (Boolean) jsonObject.get("chat_muted");
+
+            if (!chat_muted) {
+                System.out.println("Chat is enabled");
+            } else {
+                System.out.println("Chat is disabled!");
+                e.getPlayer().sendMessage("§4§lChat has been disabled.");
+                e.setCancelled(true);
+                return;
+            }
+
+
+        } catch (Exception f) {
+            f.printStackTrace();
+        }
 
             String show_color = ColorTranslator.colorTranslator.get(display.rankColor);
             System.out.println(show_color);
