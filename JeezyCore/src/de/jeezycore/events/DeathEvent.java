fa3922316923b051995e1f4edb2e912255ac7a11
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-09-11 04:55:07 +0200

    added DeathEvent class

---
+++ JeezyCore/src/de/jeezycore/events/DeathEvent.java
@@ -0,0 +1,48 @@
+package de.jeezycore.events;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.io.File;
+import java.util.List;
+
+public class DeathEvent implements Listener {
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        try {
+            File file = new File("C:\\Users\\Lassd\\IdeaProjects\\JeezyDevelopment\\JeezyCore\\src\\config.yml");
+            FileConfiguration spawn = YamlConfiguration.loadConfiguration(file);
+
+            List<Location> ls = (List<Location>) spawn.get("entry-spawn-point");
+            MemorySection mc = (MemorySection) spawn.get("spawn-settings");
+            boolean spawn_settings = mc.getBoolean("  respawn-after-death-at-spawn");
+
+
+
+            World w = ls.get(0).getWorld();
+            double x = ls.get(0).getBlockX();
+            double y = ls.get(0).getBlockY();
+            double z = ls.get(0).getBlockZ();
+            float pitch = ls.get(0).getPitch();
+            float yaw = ls.get(0).getYaw();
+            System.out.println("Here in onRespawn event");
+
+            System.out.println(spawn_settings);
+            if (!spawn_settings) {
+                return;
+            }
+            e.getEntity().teleport(new Location(w, x, y, z, pitch, yaw));
+
+        } catch (Exception f) {
+            f.printStackTrace();
+        }
+    }
+
+}
