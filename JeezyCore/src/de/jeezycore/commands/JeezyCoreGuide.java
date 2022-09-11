983126fe5c402dd8ebb1c98b152023e28bdf662f
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-09-11 05:13:46 +0200

    changed JeezyCoreGuide messages to array code looks cleaner now

--- JeezyCore/src/de/jeezycore/commands/JeezyCoreGuide.java
+++ JeezyCore/src/de/jeezycore/commands/JeezyCoreGuide.java
@@ -1,9 +1,13 @@
 package de.jeezycore.commands;
 
+import com.google.common.base.Joiner;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JeezyCoreGuide implements CommandExecutor {
 
@@ -12,15 +16,20 @@ public class JeezyCoreGuide implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (sender instanceof Player) {
-            Player player = (Player) sender;
+            Player p = (Player) sender;
 
-            player.sendMessage("§8§l§m-----------------------------------\n");
-            player.sendMessage("§b§lJeezy§9§lCore Help\n");
-            player.sendMessage("\n§f§lcreate-rank: §9§l<name> <color> <priority>\n" +
+            List<String> ls = new ArrayList<String>();
+
+
+            ls.add("§8§l§m-----------------------------------\n"+
+                    "§b§lJeezy§9§lCore Help\n"+
+                    "§f§lcreate-rank: §9§l<name> <color> <priority>\n" +
                     "§9Creates rank.\n" +
                     "§f§lgrant-rank: §9§l<player>\n" +
-                    "§9Grants player a rank.\n");
-            player.sendMessage("§8§l§m-----------------------------------");
+                    "§9Grants player a rank.\n" + "§8§l§m-----------------------------------");
+
+
+            p.sendMessage(Joiner.on(" ").join(ls));
 
 
         }
