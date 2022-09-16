e816fe1cf85e48ece67d5e9b4772c738991af0e0
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-09-17 00:49:59 +0200

    added TimeChanger class.

---
+++ JeezyCore/src/de/jeezycore/commands/TimeChanger.java
@@ -0,0 +1,26 @@
+package de.jeezycore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TimeChanger implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+
+            if (cmd.getName().equalsIgnoreCase("day")) {
+                p.getPlayer().setPlayerTime(6000, true);
+                p.sendMessage("Successfully changed time to day.");
+            } else if (cmd.getName().equalsIgnoreCase("night")) {
+                p.getPlayer().setPlayerTime(16000, true);
+                p.sendMessage("Successfully changed time to night.");
+            }
+        }
+
+        return false;
+    }
+}
