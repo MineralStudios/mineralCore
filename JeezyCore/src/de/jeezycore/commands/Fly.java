80c0e538712c616bc2fa65ca53368fd67c17303a
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-09-20 21:08:59 +0200

    added fly command.

---
+++ JeezyCore/src/de/jeezycore/commands/Fly.java
@@ -0,0 +1,52 @@
+package de.jeezycore.commands;
+
+import de.jeezycore.utils.PermissionHandler;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
+import static de.jeezycore.utils.ArrayStorage.fly_array;
+
+public class Fly implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+
+
+            if (cmd.getName().equalsIgnoreCase("fly")) {
+                if (p.hasPermission("jeezyfly")) {
+
+                    System.out.println(fly_array.contains(p.getPlayer().getDisplayName()));
+                    System.out.println(fly_array);
+
+                    if (!fly_array.contains(p.getPlayer().getDisplayName())) {
+                        p.setAllowFlight(true);
+                        p.setFlying(true);
+                        p.sendMessage("Fly on");
+                        fly_array.add(p.getPlayer().getDisplayName());
+                    } else {
+                        p.setAllowFlight(false);
+                        p.setFlying(false);
+                        p.sendMessage("Fly off");
+                        fly_array.remove(p.getPlayer().getDisplayName());
+                    }
+
+                } else {
+                    p.sendMessage("No permission.");
+                }
+            }
+
+
+
+        }
+
+        return false;
+    }
+}
