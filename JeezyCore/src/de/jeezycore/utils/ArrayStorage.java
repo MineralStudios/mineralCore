5cb8bb2ebb7ed372cbeb6bb93941777a8e78516d
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-09-20 21:08:48 +0200

    added ArrayStorage class.

---
+++ JeezyCore/src/de/jeezycore/utils/ArrayStorage.java
@@ -0,0 +1,13 @@
+package de.jeezycore.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ArrayStorage {
+    // Msg command array
+    public static HashMap<String, String> reply_array = new HashMap<String, String>();
+
+    // fly command array
+    public  static ArrayList<String> fly_array = new ArrayList<String>();
+
+}
