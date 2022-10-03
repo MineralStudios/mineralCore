66ab9b3b9449f4088b685a9ed17190edfc3d9652
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-10-03 18:50:06 +0200

    added a bunch of new menus and items to InventoryClickEvent

--- JeezyCore/src/de/jeezycore/events/InventoryClickEvent.java
+++ JeezyCore/src/de/jeezycore/events/InventoryClickEvent.java
@@ -7,6 +7,7 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
@@ -15,6 +16,9 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 
 public class InventoryClickEvent implements Listener {
+
+    Inventory profile_inv;
+    Inventory manage_menu;
 
     @EventHandler
     public void onCLickEvent(org.bukkit.event.inventory.InventoryClickEvent e) {
@@ -33,7 +37,7 @@ public class InventoryClickEvent implements Listener {
             e.getWhoClicked().sendMessage("You §b§lsuccessfully§f granted §l§7"+ UUIDChecker.uuidName +"§f the §l"+e.getCurrentItem().getItemMeta().getDisplayName()+" §frank.");
 
         } else if(e.getClickedInventory().getName().contains("Grant") && e.getCurrentItem().getItemMeta().getDisplayName().contains(UUIDChecker.uuidName)) {
-            Inventory inv = Bukkit.createInventory(null, 27,"§8Profile: " +"§f§l"+UUIDChecker.uuidName);
+            profile_inv = Bukkit.createInventory(null, 27,"§8Profile: " +"§f§l"+UUIDChecker.uuidName);
             ItemStack back = new ItemStack(Material.REDSTONE);
             ItemStack manage_rank = new ItemStack(Material.REDSTONE_BLOCK);
             ItemStack punishments = new ItemStack(Material.WATCH);
@@ -46,10 +50,10 @@ public class InventoryClickEvent implements Listener {
             back.setItemMeta(backm);
             manage_rank.setItemMeta(manage_rankm);
             punishments.setItemMeta(punishmentsm);
-            inv.setItem(9, back);
-            inv.setItem(13, manage_rank);
-            inv.setItem(17, punishments);
-            e.getWhoClicked().openInventory(inv);
+            profile_inv.setItem(9, back);
+            profile_inv.setItem(13, manage_rank);
+            profile_inv.setItem(17, punishments);
+            e.getWhoClicked().openInventory(profile_inv);
             e.setCancelled(true);
         }
 
@@ -57,9 +61,65 @@ public class InventoryClickEvent implements Listener {
             e.setCancelled(true);
             e.getWhoClicked().openInventory(GrantRank.grant_inv);
         } else if (e.getClickedInventory().getName().contains("Profile") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§3Manage")) {
+            manage_menu = Bukkit.createInventory(null, 27,"§8Manage Menu");
+            ItemStack remove_rank = new ItemStack(Material.EMERALD);
+            ItemMeta remove_rankm = remove_rank.getItemMeta();
+            remove_rankm.setDisplayName("§4Rank remove");
+            remove_rank.setItemMeta(remove_rankm);
+
+            ItemStack go_back = new ItemStack(Material.REDSTONE);
+            ItemMeta go_backm = remove_rank.getItemMeta();
+            go_backm.setDisplayName("§cBack");
+            go_back.setItemMeta(go_backm);
+
+            manage_menu.setItem(9, go_back);
+            manage_menu.setItem(13, remove_rank);
+            e.getWhoClicked().openInventory(manage_menu);
             e.setCancelled(true);
         } else if(e.getClickedInventory().getName().contains("Profile") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4Punishments")) {
             e.setCancelled(true);
+        }
+
+        if (e.getClickedInventory().getName().contains("Manage Menu") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§cBack")) {
+            e.setCancelled(true);
+            e.getWhoClicked().openInventory(profile_inv);
+        } else if(e.getClickedInventory().getName().contains("Manage Menu") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4Rank remove")) {
+            Inventory inv = Bukkit.createInventory(null, 27,"§8Remove Rank: §f§l"+UUIDChecker.uuidName);
+            ItemStack go_back = new ItemStack(Material.REDSTONE);
+            ItemMeta go_backm = go_back.getItemMeta();
+            go_backm.setDisplayName("§cBack");
+
+
+            ItemStack remove_yes = new ItemStack(Material.WOOL, 1, (short) 5);
+            ItemMeta remove_yesm = remove_yes.getItemMeta();
+            remove_yesm.setDisplayName("§aYes");
+
+            ItemStack remove_no = new ItemStack(Material.WOOL, 1, (short) 14);
+            ItemMeta remove_nom = remove_no.getItemMeta();
+            remove_nom.setDisplayName("§4No");
+
+            go_back.setItemMeta(go_backm);
+            remove_yes.setItemMeta(remove_yesm);
+            remove_no.setItemMeta(remove_nom);
+
+            inv.setItem(9, go_back);
+            inv.setItem(12, remove_yes);
+            inv.setItem(14, remove_no);
+            e.getWhoClicked().openInventory(inv);
+            e.setCancelled(true);
+        } else if(e.getClickedInventory().getName().contains("§8Remove Rank: §f§l"+UUIDChecker.uuidName) && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§cBack")) {
+            e.setCancelled(true);
+            e.getWhoClicked().openInventory(manage_menu);
+        }
+
+        if (e.getClickedInventory().getName().contains("§8Remove Rank: §f§l"+UUIDChecker.uuidName) && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§aYes")) {
+            e.setCancelled(true);
+            JeezySQL removeRank = new JeezySQL();
+            removeRank.removeRankGui((Player) e.getWhoClicked());
+            e.getWhoClicked().closeInventory();
+        } else if (e.getClickedInventory().getName().contains("§8Remove Rank: §f§l"+UUIDChecker.uuidName) && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4No")) {
+            e.setCancelled(true);
+            e.getWhoClicked().closeInventory();
         }
 
 
