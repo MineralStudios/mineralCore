f0d43c23e3618e6ca0d1c2a1191eb946a20d4ab2
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-10-16 01:40:42 +0200

    added spawn command that is configurable

---
+++ JeezyCore/src/de/jeezycore/commands/Spawn.java
@@ -0,0 +1,53 @@
+package de.jeezycore.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.util.List;
+
+public class Spawn implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (cmd.getName().equalsIgnoreCase("spawn")) {
+
+                File file = new File("C:\\Users\\Lassd\\IdeaProjects\\JeezyDevelopment\\JeezyCore\\src\\config.yml");
+                FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+                List<Location> ls = (List<Location>) config.get("entry-spawn-point");
+
+                MemorySection mc = (MemorySection) config.get("spawn-settings");
+                boolean spawn_command = mc.getBoolean("spawn-command");
+
+                if (!spawn_command) {
+                    return true;
+                }
+
+                World w = ls.get(0).getWorld();
+                double x = ls.get(0).getBlockX();
+                double y = ls.get(0).getBlockY();
+                double z = ls.get(0).getBlockZ();
+                float yaw = ls.get(0).getYaw();
+                float pitch = ls.get(0).getPitch();
+
+
+                p.teleport(new Location(w, x, y, z, yaw, pitch));
+            }
+
+
+        }
+
+        return false;
+    }
+}
