eed13b36f04372a916781ddf49fc36005b3312e2
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-10-17 00:38:31 +0200

    added BotStatus class to set bot status

---
+++ JeezyCore/src/main/java/de/jeezycore/discord/activity/BotStatus.java
@@ -0,0 +1,11 @@
+package de.jeezycore.discord.activity;
+
+import static de.jeezycore.discord.JeezyBot.api;
+
+public class BotStatus {
+
+    public void set () {
+        api.updateActivity("JeezyCore 🎮");
+    }
+
+}
