3d7de7eba5a7b80d153a6b09131829d45acfb36d
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-10-17 00:39:19 +0200

    set allIntents in JeezyBot class and called botStatus and registered event.

--- JeezyCore/src/main/java/de/jeezycore/discord/JeezyBot.java
+++ JeezyCore/src/main/java/de/jeezycore/discord/JeezyBot.java
@@ -1,5 +1,7 @@
 package de.jeezycore.discord;
 
+import de.jeezycore.discord.activity.BotStatus;
+import de.jeezycore.discord.events.MessageCreate;
 import org.bukkit.configuration.MemorySection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -10,8 +12,9 @@ import java.io.File;
 
 public class JeezyBot {
 
-    public void start() {
+    public static DiscordApi api;
 
+    public void start() {
         File file = new File("C:\\Users\\Lassd\\IdeaProjects\\JeezyCore\\JeezyCore\\src\\main\\java\\discord.yml");
         FileConfiguration config = YamlConfiguration.loadConfiguration(file);
 
@@ -19,9 +22,15 @@ public class JeezyBot {
         MemorySection discord = (MemorySection) config.get("discord-settings");
         String token = (String) discord.get("token");
 
-        DiscordApi api = new DiscordApiBuilder()
+         api = new DiscordApiBuilder()
                 .setToken(token)
+                .setAllIntents()
                 .login().join();
-    }
 
-}
+        BotStatus botStatus = new BotStatus();
+        botStatus.set();
+
+        // Register EventListener
+        api.addListener(new MessageCreate());
+    }
+    }
