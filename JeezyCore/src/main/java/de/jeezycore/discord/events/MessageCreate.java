c9e3a62b5634f48e4e7dd11278d898ca7b7b19be
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-10-17 22:31:22 +0200

    implemented realtimeChatViaDiscord in MessageCreate to make discord to mc happen

--- JeezyCore/src/main/java/de/jeezycore/discord/events/MessageCreate.java
+++ JeezyCore/src/main/java/de/jeezycore/discord/events/MessageCreate.java
@@ -1,7 +1,7 @@
 package de.jeezycore.discord.events;
 
 
-import org.javacord.api.entity.channel.TextChannel;
+import de.jeezycore.discord.chat.RealtimeChat;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
@@ -9,9 +9,11 @@ import org.javacord.api.listener.message.MessageCreateListener;
 public class MessageCreate implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-
-        System.out.println(event.getMessageContent());
+        RealtimeChat discord_realtime = new RealtimeChat();
+        if (event.getMessage().getUserAuthor().get().isBot()) {
+            return;
+        }
+        discord_realtime.realtimeChatViaDiscord("§7[§9Discord§7]§f "+event.getMessage().getUserAuthor().get().getName()+": "+event.getMessageContent());
 
     }
-
 }
