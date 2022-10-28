5cfb57a85253b6aa88f9e4c198ea199e15ce91cf
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-10-28 18:02:02 +0200

    changed ban command initialisation to new SQL class called BanSQL

--- JeezyCore/src/main/java/de/jeezycore/commands/punishments/Ban.java
+++ JeezyCore/src/main/java/de/jeezycore/commands/punishments/Ban.java
@@ -1,15 +1,11 @@
 package de.jeezycore.commands.punishments;
 
 import com.google.common.base.Joiner;
-import de.jeezycore.db.JeezySQL;
-import de.jeezycore.utils.ArrayStorage;
-import de.jeezycore.utils.UUIDChecker;
-import org.bukkit.Bukkit;
+import de.jeezycore.db.BanSQL;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.json.simple.JSONObject;
 
 import java.util.*;
 
@@ -27,8 +23,8 @@ public class Ban implements CommandExecutor {
                         .skipNulls()
                         .join(ls).replace(args[0], "");
 
-                JeezySQL banSQL = new JeezySQL();
-                banSQL.ban(args[0], input, p.getPlayer());
+                BanSQL execute = new BanSQL();
+                execute.ban(args[0], input, p.getPlayer());
 
             } else {
                 p.sendMessage("Usage: /ban <player><reason>.");
