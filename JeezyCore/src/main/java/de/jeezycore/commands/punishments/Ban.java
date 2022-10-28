4186a4c5137757bcaa5336d9a87341adcee7eb36
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-10-28 17:50:24 +0200

    cleaned up Ban command.

--- JeezyCore/src/main/java/de/jeezycore/commands/punishments/Ban.java
+++ JeezyCore/src/main/java/de/jeezycore/commands/punishments/Ban.java
@@ -27,36 +27,8 @@ public class Ban implements CommandExecutor {
                         .skipNulls()
                         .join(ls).replace(args[0], "");
 
-                String sql = "INSERT INTO punishments " +
-                        "(UUID, banned_forever, ban_time, mute_time, punishment_log) " +
-                        "VALUES " +
-                        "(?, true, NULL, NULL, ?)";
-
                 JeezySQL banSQL = new JeezySQL();
-                UUIDChecker uc = new UUIDChecker();
-                uc.check(args[0]);
-
-                JSONObject json_o = new JSONObject();
-
-                json_o.put("banned by", UUIDChecker.uuid);
-                json_o.put("time", "forever");
-                json_o.put("reason", input);
-                ArrayStorage.punishment_log.add(json_o);
-
-                banSQL.ban(sql, UUID.fromString(UUIDChecker.uuid), ArrayStorage.punishment_log.toString());
-
-                System.out.println(ArrayStorage.punishment_log);
-
-                p.sendMessage("§7You §asuccessfully §7banned §4"+ args[0]+" §7from the server.");
-
-                try {
-                    System.out.println(UUIDChecker.uuid);
-                    Bukkit.getServer().getPlayer(UUID.fromString(UUIDChecker.uuid)).kickPlayer("§4You are permanently banned from JeezyDevelopment.\n\n" +
-                            "§6Reason: §c" +input+"§7.\n\n"+
-                            "§7If you feel this ban has been unjustified, appeal on our discord at\n §bjeezydevelopment.com§7.");
-                } catch (Exception e) {
-                    return true;
-                }
+                banSQL.ban(args[0], input, p.getPlayer());
 
             } else {
                 p.sendMessage("Usage: /ban <player><reason>.");
