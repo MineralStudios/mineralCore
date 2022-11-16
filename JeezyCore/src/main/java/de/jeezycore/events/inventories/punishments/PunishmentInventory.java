0fa3db984b93d3cee57ba19178b4a0e4423616bb
JeezyDev <JeffreyEjeanye@gmail.com>
2022-11-16 14:56:55 +0100

    cleaned PunishmentInventory and thrown out other inventories

--- JeezyCore/src/main/java/de/jeezycore/events/inventories/punishments/PunishmentInventory.java
+++ JeezyCore/src/main/java/de/jeezycore/events/inventories/punishments/PunishmentInventory.java
@@ -8,18 +8,11 @@ import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 public class PunishmentInventory {
 
     Inventory punishment_inventory;
-    Inventory ban_inventory;
 
     public void run(org.bukkit.event.inventory.InventoryClickEvent e) {
         if(e.getClickedInventory().getName().contains("Profile") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4Punishments")) {
@@ -30,63 +23,10 @@ public class PunishmentInventory {
         }else if(e.getClickedInventory().getName().contains("Punishments") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§cBack")) {
             e.getWhoClicked().openInventory(ArrayStorage.profile_inv_array.get(e.getWhoClicked().getName()));
             e.setCancelled(true);
-        } else if(e.getClickedInventory().getName().contains("Punishments") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4§lBans")) {
-            displayBanPunishments(e);
-        }  else if(e.getClickedInventory().getName().contains("Punishments") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§4§lBans")) {
-            e.setCancelled(true);
         }
-
         if(e.getClickedInventory().getName().contains("Punishments") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§cBack")) {
             e.getWhoClicked().openInventory(ArrayStorage.profile_inv_array.get(e.getWhoClicked().getName()));
             e.setCancelled(true);
-        } else if(e.getClickedInventory().getName().contains("Bans") && e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§cBack")) {
-            e.getWhoClicked().openInventory(ArrayStorage.punishments_menu_inv_array.get(e.getWhoClicked().getName()));
-            e.setCancelled(true);
-        }
-
-
-        if (e.getClickedInventory().getName().contains("Bans") && e.getCurrentItem().getData().toString().contains("PAPER")) {
-            e.setCancelled(true);
-        }
-
-    }
-
-    private void displayBanPunishments(org.bukkit.event.inventory.InventoryClickEvent e) {
-        try {
-            createBanInventory(e);
-            int num = 1;
-            JSONParser jsParser = new JSONParser();
-            JSONArray jsonA = (JSONArray) jsParser.parse(LogsSQL.ban_log);
-            for (int i = 0; i < jsonA.size(); i++) {
-
-                JSONObject jsonOB = (JSONObject) jsParser.parse(jsonA.get(i).toString());
-
-                ItemStack rank = new ItemStack(Material.PAPER, 1);
-                ItemStack back = new ItemStack(Material.REDSTONE);
-                ItemMeta backm = back.getItemMeta();
-                backm.setDisplayName("§cBack");
-                back.setItemMeta(backm);
-                String displayName = "§4#§7"+0+0+num++;
-                ItemMeta rankMeta = rank.getItemMeta();
-                List<String> desc = new ArrayList<String>();
-                desc.add(0, "§8§m-----------------------------------");
-                desc.add(1, "§4UUID: §7"+UUIDChecker.uuid+"");
-                desc.add(2,"§4Reason: §7"+jsonOB.get("reason"));
-                desc.add(3,"§4ban_start: §7"+jsonOB.get("ban_start"));
-                desc.add(4,"§4ban_end: §7"+jsonOB.get("ban_end"));
-                desc.add(5,"§4banned_by: §7"+jsonOB.get("banned by"));
-                desc.add(6, "§8§m-----------------------------------");
-                rankMeta.setDisplayName(displayName);
-                rankMeta.setLore(desc);
-                rank.setItemMeta(rankMeta);
-                ban_inventory.setItem(0, back);
-                ban_inventory.setItem(i+1, rank);
-
-            }
-            e.getWhoClicked().openInventory(ban_inventory);
-            e.setCancelled(true);
-        } catch (Exception err) {
-            err.printStackTrace();
         }
     }
 
@@ -99,7 +39,7 @@ public class PunishmentInventory {
         ItemMeta mutesm = mutes.getItemMeta();
         backm.setDisplayName("§cBack");
         bansm.setDisplayName("§4§lBans");
-        mutesm.setDisplayName("§1§lMutes");
+        mutesm.setDisplayName("§9§lMutes");
         back.setItemMeta(backm);
         bans.setItemMeta(bansm);
         mutes.setItemMeta(mutesm);
@@ -117,10 +57,6 @@ public class PunishmentInventory {
         logs.punishment_log(UUID.fromString(UUIDChecker.uuid));
     }
 
-    private void createBanInventory(org.bukkit.event.inventory.InventoryClickEvent e) {
-        ban_inventory = Bukkit.createInventory(null, 27,"§8Bans: §f§l"+ ArrayStorage.grant_array_names.get(e.getWhoClicked().getUniqueId()));
-    }
-
     private void createPunishmentInventory(org.bukkit.event.inventory.InventoryClickEvent e) {
         punishment_inventory = Bukkit.createInventory(null, 27,"§8Punishments: §f§l"+ ArrayStorage.grant_array_names.get(e.getWhoClicked().getUniqueId()));
     }
@@ -128,5 +64,4 @@ public class PunishmentInventory {
     private void putArray(org.bukkit.event.inventory.InventoryClickEvent e) {
         ArrayStorage.punishments_menu_inv_array.put(e.getWhoClicked().getName(), punishment_inventory);
     }
-
 }
