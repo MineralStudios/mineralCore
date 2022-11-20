1301a601d26dfbda7741d108953e983642fa3692
JeezyDev <JeffreyEjeanye@gmail.com>
2022-11-20 13:43:00 +0100

    created StaffChat class

---
+++ JeezyCore/src/main/java/de/jeezycore/events/chat/StaffChat.java
@@ -0,0 +1,42 @@
+package de.jeezycore.events.chat;
+
+import de.jeezycore.colors.ColorTranslator;
+import de.jeezycore.db.JeezySQL;
+import de.jeezycore.db.StaffSQL;
+import org.bukkit.Bukkit;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.UUID;
+
+public class StaffChat {
+
+    public void chat(AsyncPlayerChatEvent e) {
+        String chat_message = e.getMessage();
+        String firstChar = String.valueOf(chat_message.charAt(0));
+        String expectedOutput = "@";
+
+        StaffSQL staffSQL = new StaffSQL();
+        staffSQL.checkIfStaff(UUID.fromString(e.getPlayer().getUniqueId().toString()));
+
+        if (StaffSQL.staffRank && firstChar.equalsIgnoreCase(expectedOutput)) {
+            staffSQL.getStaff(e);
+
+                JeezySQL display = new JeezySQL();
+                String sql = "SELECT * FROM jeezycore WHERE playerName LIKE '%"+ e.getPlayer().getUniqueId().toString() +"%'";
+                display.displayChatRank(sql);
+
+                try {
+                    for (int i = 0; i < StaffSQL.staff.size(); i++) {
+                        String new_message = e.getMessage().replace("@", "").trim();
+                        Bukkit.getPlayer(UUID.fromString(StaffSQL.staff.get(i))).sendMessage("§7§l[§bStaff§7-§bChat§7§l] "+ColorTranslator.colorTranslator.get(display.rankColor)+e.getPlayer().getDisplayName()+"§f: "+new_message);
+                        e.setCancelled(true);
+                    }
+                    StaffSQL.staffRank = false;
+                    StaffSQL.staffPlayerNames = null;
+                    StaffSQL.staff.clear();
+                } catch (Exception f) {
+                    f.printStackTrace();
+                }
+        }
+    }
+}
