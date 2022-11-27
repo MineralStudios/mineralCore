44abd7920c83c8efa84e73c375c3f3926e615b19
JeezyDev <JeffreyEjeanye@gmail.com>
2022-11-28 00:11:09 +0100

    created GrantTag class (works)

---
+++ JeezyCore/src/main/java/de/jeezycore/commands/tags/GrantTag.java
@@ -0,0 +1,34 @@
+package de.jeezycore.commands.tags;
+
+import de.jeezycore.db.TagsSQL;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GrantTag implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+
+            if (cmd.getName().equalsIgnoreCase("grant-tag") && args.length == 2) {
+                if (p.hasPermission("jeezy.core.tags.grant")) {
+                    TagsSQL mySQL = new TagsSQL();
+                    String input = "INSERT INTO tags " +
+                            "(tagName, tagDesign, tagPriority) " +
+                            "VALUES " +
+                            "(?, ?, ?)";
+
+                    mySQL.grantTag(p, args[0], args[1]);
+                } else {
+                    p.sendMessage("No permission.");
+                }
+            } else {
+                p.sendMessage("Usage: /grant-tag (tagName) (playerName)");
+            }
+        }
+        return true;
+    }
+}
