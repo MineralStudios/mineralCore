04ddddc460764b756a74e825686c6f7716749110
JeezyDev <JeffreyEjeanye@gmail.com>
2022-12-03 18:26:27 +0100

    UUIDChecker fixed simplified it and removed for loop.

--- JeezyCore/src/main/java/de/jeezycore/utils/UUIDChecker.java
+++ JeezyCore/src/main/java/de/jeezycore/utils/UUIDChecker.java
@@ -11,33 +11,27 @@ import java.net.URL;
 public class UUIDChecker {
         public static String uuid;
         public static String uuidName;
-        public void check(String userName)  {
 
-                for (Player ps : Bukkit.getOnlinePlayers()) {
-                        if (ps.getName().equalsIgnoreCase(userName)) {
-                                 uuid = ps.getUniqueId().toString().replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
-                                 uuidName = ps.getPlayer().getDisplayName();
-                                 System.out.println(uuid);
-                        } else {
-                                try {
-                                        String user = userName;
-                                        String url = "https://api.mojang.com/users/profiles/minecraft/"+user;
-                                        String UUIDJson = IOUtils.toString(new URL(url));
-                                        JSONObject UUIDObject = (JSONObject) JSONValue.parseWithException(UUIDJson);
-                                        uuid = UUIDObject.get("id").toString().replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
-                                        uuidName = UUIDObject.get("name").toString();
-                                        System.out.println(uuid);
-                                        System.out.println(uuidName);
-                                } catch (Exception e) {
-                                   System.out.println(e);
-                                }
+        public void check(String userName) {
 
+                Player ps = Bukkit.getPlayer(userName);
 
+                if (ps != null) {
+                        uuid = ps.getUniqueId().toString().replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+                        uuidName = ps.getPlayer().getDisplayName();
+                        System.out.println(uuid);
+                        return;
+                }
+                        try {
+                                String url = "https://api.mojang.com/users/profiles/minecraft/" + userName;
+                                String UUIDJson = IOUtils.toString(new URL(url));
+                                JSONObject UUIDObject = (JSONObject) JSONValue.parseWithException(UUIDJson);
+                                uuid = UUIDObject.get("id").toString().replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+                                uuidName = UUIDObject.get("name").toString();
+                                System.out.println(uuid);
+                                System.out.println(uuidName);
+                        } catch (Exception e) {
+                                System.out.println(e);
                         }
                 }
-
-
         }
-
-
-}
