5a435d0eb01c00992e9dd30830f5041f46429bf4
JeezyDev <JeffreyEjeanye@gmail.com>
2022-12-11 22:52:13 +0100

    added TabListSQL to get the color code from player

---
+++ JeezyCore/src/main/java/de/jeezycore/db/TabListSQL.java
@@ -0,0 +1,48 @@
+package de.jeezycore.db;
+
+import de.jeezycore.config.JeezyConfig;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.entity.Player;
+
+import java.sql.*;
+
+import static de.jeezycore.utils.ArrayStorage.tab_name_list_array;
+
+public class TabListSQL {
+    public String url;
+    public String user;
+    public String password;
+    int rankColor;
+
+    private void createConnection() {
+        MemorySection mc = (MemorySection) JeezyConfig.database_defaults.get("MYSQL");
+        url = "jdbc:mysql://"+mc.get("ip")+":"+mc.get("mysql-port")+"/"+mc.get("database");
+        user = (String) mc.get("user");
+        password = (String) mc.get("password");
+    }
+
+
+    public void getTabListData(Player p) {
+        try {
+            this.createConnection();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+            String sql = "SELECT * FROM jeezycore WHERE playerName LIKE '%"+p.getPlayer().getUniqueId()+"%'";
+            ResultSet rs = stm.executeQuery(sql);
+            while(rs.next()){
+                rankColor = rs.getInt(2);
+
+                if (rankColor == 0) {
+                return;
+                }
+
+                tab_name_list_array.put(p, rankColor);
+            }
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+
+}
