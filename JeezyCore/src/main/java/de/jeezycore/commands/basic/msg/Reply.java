95e2923fd2751f236baac24fa53f25e8df774c1b
JeezyDev <JeffreyEjeanye@gmail.com>
2023-01-20 18:21:13 +0100

    Put Reply to a separate class

---
+++ JeezyCore/src/main/java/de/jeezycore/commands/basic/msg/Reply.java
@@ -0,0 +1,58 @@
+package de.jeezycore.commands.basic.msg;
+
+import com.google.common.base.Joiner;
+import de.jeezycore.db.JeezySQL;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static de.jeezycore.utils.ArrayStorage.msg_ignore_list;
+import static de.jeezycore.utils.ArrayStorage.reply_array;
+
+public class Reply implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            List<String> ls = new ArrayList<String>(Arrays.asList(args));
+
+            if (cmd.getName().equalsIgnoreCase("r") && args.length != 0) {
+                String result = reply_array.get(p.getPlayer().getDisplayName());
+                System.out.println(result);
+                if (result == null || Bukkit.getServer().getPlayerExact(result) == null) {
+                    p.sendMessage("§cThere is nobody to reply to.");
+                    return true;
+                }
+
+                if (msg_ignore_list.get(Bukkit.getPlayer(result).getUniqueId()) != null) {
+                    if (msg_ignore_list.get(Bukkit.getPlayer(result).getUniqueId()).contains(p.getPlayer().getUniqueId().toString())) {
+                        p.sendMessage("§9"+result+" §7has ignored you.");
+                        return true;
+                    }
+                }
+
+                String input = Joiner.on(" ")
+                        .skipNulls()
+                        .join(ls);
+                JeezySQL display = new JeezySQL();
+                String sql = "SELECT * FROM jeezycore WHERE playerName LIKE '%"+ p.getPlayer().getUniqueId().toString() +"%'";
+                display.displayChatRank(sql);
+
+                Bukkit.getPlayer(result).sendMessage("§8§l(§4§lmsg§8§l) "+display.rankColor.replace("&", "§")+p.getPlayer().getDisplayName()+"§7: "+input);
+                reply_array.put(result, p.getPlayer().getDisplayName());
+                p.sendMessage("§8§l(§4§lreplied§8§l) "+display.rankColor.replace("&", "§")+p.getPlayer().getDisplayName()+"§7: "+input);
+
+            } else if (cmd.getName().equalsIgnoreCase("r") && args.length == 0) {
+                p.sendMessage("Usage /r <message>");
+            }
+        }
+        return true;
+    }
+}
