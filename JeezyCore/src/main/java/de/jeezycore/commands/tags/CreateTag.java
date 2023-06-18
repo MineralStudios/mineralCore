68e2806e9a2f5fd884a9054d89fe5b534f8b67f9
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-06-18 22:22:22 +0200

    added tagsCategory to CreateTag.

--- JeezyCore/src/main/java/de/jeezycore/commands/tags/CreateTag.java
+++ JeezyCore/src/main/java/de/jeezycore/commands/tags/CreateTag.java
@@ -1,13 +1,19 @@
 package de.jeezycore.commands.tags;
 
 import de.jeezycore.colors.ColorTranslator;
+import de.jeezycore.config.JeezyConfig;
 import de.jeezycore.db.TagsSQL;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.MemorySection;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class CreateTag implements CommandExecutor {
+
+    List<Object> tag_categories_list = (List<Object>) JeezyConfig.config_defaults.get("tags.categories");
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -15,15 +21,19 @@ public class CreateTag implements CommandExecutor {
             Player p = (Player) sender;
 
            if (cmd.getName().equalsIgnoreCase("create-tag") && args.length < 3) {
-              p.sendMessage("Usage: /create-tag (name) (design) (priority)");
+              p.sendMessage("Usage: /create-tag (name) (category) (design) (priority)");
            } else {
                if (p.hasPermission("jeezy.core.tags.create")) {
                    TagsSQL mySQL = new TagsSQL();
                    String input = "INSERT INTO tags " +
-                           "(tagName, tagDesign, tagPriority) " +
+                           "(tagName, tagCategory, tagDesign, tagPriority) " +
                            "VALUES " +
-                           "(?, ?, ?)";
-                   mySQL.pushData(input, p, args[0], args[1], args[2]);
+                           "(?, ?, ?, ?)";
+                   if (tag_categories_list.contains(args[1])) {
+                       mySQL.pushData(input, p, args[0], args[1], args[2], args[3]);
+                   } else {
+                       return true;
+                   }
                } else {
                    p.sendMessage("No permission.");
                }
