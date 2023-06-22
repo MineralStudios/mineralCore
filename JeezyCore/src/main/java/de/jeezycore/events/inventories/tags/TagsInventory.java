77da258f396bba9eccc620afe7045375c6b65d22
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-06-22 22:48:28 +0200

    fixed up whole TagsInventory class to adapt with new displaying methode called arraylist.

--- JeezyCore/src/main/java/de/jeezycore/events/inventories/tags/TagsInventory.java
+++ JeezyCore/src/main/java/de/jeezycore/events/inventories/tags/TagsInventory.java
@@ -26,11 +26,11 @@ public class TagsInventory {
 
     public void run(org.bukkit.event.inventory.InventoryClickEvent e) {
         if (e.getInventory().getTitle().contains("§8§lTags")) {
-            if (e.getCurrentItem().getData().toString().equalsIgnoreCase("NAME_TAG(0)") && e.getCurrentItem().getItemMeta().getLore().get(3).equalsIgnoreCase("§a§lYou own this tag§7§l.")) {
+            if (e.getCurrentItem().getData().toString().equalsIgnoreCase("NAME_TAG(0)") && e.getCurrentItem().getItemMeta().getLore().get(5).equalsIgnoreCase("§a§lYou own this tag§7§l.")) {
                 e.getWhoClicked().sendMessage("§7You §2§lsuccessfully §7gave yourself the §9§l"+e.getCurrentItem().getItemMeta().getDisplayName()+ " §7tag§7.");
                 e.getWhoClicked().closeInventory();
-                executeMYSQL(e.getCurrentItem().getItemMeta().getDisplayName(), e.getWhoClicked().getUniqueId());
-            } else if (e.getCurrentItem().getData().toString().equalsIgnoreCase("NAME_TAG(0)") && e.getCurrentItem().getItemMeta().getLore().get(3).equalsIgnoreCase("§4§lYou don't own this tag yet§7§l.")) {
+                executeMYSQL(e.getCurrentItem().getItemMeta().getDisplayName().substring(4), e.getWhoClicked().getUniqueId());
+            } else if (e.getCurrentItem().getData().toString().equalsIgnoreCase("NAME_TAG(0)") && e.getCurrentItem().getItemMeta().getLore().get(5).equalsIgnoreCase("§4§lYou don't own this tag yet§7§l.")) {
                 e.getWhoClicked().sendMessage("§4§lYou don't own this tag.");
             }
 
@@ -112,47 +112,54 @@ public class TagsInventory {
 
         }
 
-        int i = 0;
-            for (Map.Entry<String, String> entry : display.tagData.entrySet()) {
+
+
+            for (int i = 0; i < display.arrayList.size(); i++) {
+
+                String test = display.arrayList.get(i).replace("[", "").replace(",", "").replace("]", "");
+
                 ItemStack tag = new ItemStack(Material.NAME_TAG, 1);
-                String tagName = entry.getKey();
-                String tagFormat = entry.getValue();
+                String tagName = test.split(" ")[0];
+                String tagCategory = test.split(" ")[1];
+                String tagFormat = test.split(" ")[2];
                 ItemMeta tagMeta = tag.getItemMeta();
                 List<String> desc = new ArrayList<String>();
                 desc.add(0, "§8§m-----------------------------------");
-                desc.add(1, "§9Format: " + tagFormat.replaceAll("&", "§"));
-                desc.add(2, "§8§m-----------------------------------");
-                desc.add(3, "§4§lYou don't own this tag yet§7§l.");
-                if (i == 7 || i == 14)  {
-                    addUp +=2;
+                desc.add(1, "§7§lCategory: §f§l" + tagCategory);
+                desc.add(2, "");
+                desc.add(3, "§7§lDisplay: §2" + p.getDisplayName() + " " + tagFormat.replaceAll("&", "§"));
+                desc.add(4, "§8§m-----------------------------------");
+                desc.add(5, "§4§lYou don't own this tag yet§7§l.");
+                if (i == 7 || i == 14) {
+                    addUp += 2;
                 } else if (i >= 21) {
                     break;
                 }
 
 
-                if (entry.getKey().equalsIgnoreCase(RewardSQL.rewardPrice)) {
+                if (display.arrayList.get(i).equalsIgnoreCase(RewardSQL.rewardPrice)) {
                     desc.remove(3);
                     desc.add(3, "§a§lYou own this tag§7§l.");
                 }
 
                 if (tags_in_ownership_array.size() != 0) {
                     for (int x = 0; x < tags_in_ownership_array.size(); x++) {
-                        if (entry.getKey().equalsIgnoreCase(tags_in_ownership_array.get(x))) {
-                            desc.remove(3);
-                            desc.add(3, "§a§lYou own this tag§7§l.");
+                        if (display.arrayList.get(i).contains(tags_in_ownership_array.get(x))) {
+                            desc.remove(5);
+                            desc.add(5, "§a§lYou own this tag§7§l.");
                             break;
                         }
                     }
                 }
-               tagMeta.setDisplayName(tagName);
-               tagMeta.setLore(desc);
-               tag.setItemMeta(tagMeta);
-               tag_inv.setItem(addUp+ i, tag);
+                tagMeta.setDisplayName("§9§l"+tagName);
+                tagMeta.setLore(desc);
+                tag.setItemMeta(tagMeta);
+                tag_inv.setItem(addUp + i, tag);
 
-                i++;
+
+                p.openInventory(tag_inv);
+                RewardSQL.rewardPrice = null;
             }
-        p.openInventory(tag_inv);
-        RewardSQL.rewardPrice = null;
     }
 
     public void executeMYSQL(String tagName, UUID p) {
