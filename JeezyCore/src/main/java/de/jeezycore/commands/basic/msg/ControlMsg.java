678d3cb3c671e21843952a82e2e66dc8855490b1
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-07-11 01:46:06 +0200

    changed tpm command now checking with db data instead of array.

--- JeezyCore/src/main/java/de/jeezycore/commands/basic/msg/ControlMsg.java
+++ JeezyCore/src/main/java/de/jeezycore/commands/basic/msg/ControlMsg.java
@@ -6,12 +6,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.UUID;
-
 public class ControlMsg implements CommandExecutor {
 
-    ArrayList<UUID> users = new ArrayList<UUID>();
     SettingsSQL settingsSQL = new SettingsSQL();
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -20,13 +16,11 @@ public class ControlMsg implements CommandExecutor {
             Player p = (Player) sender;
 
         if (cmd.getName().equalsIgnoreCase("tpm") && args.length == 0) {
-
-            if (users.contains(p.getUniqueId())) {
-                users.remove(p.getUniqueId());
+            settingsSQL.getSettingsData(p.getUniqueId());
+            if (settingsSQL.settingsMsg) {
                 settingsSQL.disableMsg(p);
                 p.sendMessage("§7You successfully §cdisabled §7privat messages.");
             } else {
-                users.add(p.getUniqueId());
                 settingsSQL.enableMsg(p);
                 p.sendMessage("§7You successfully §2enabled §7privat messages.");
             }
