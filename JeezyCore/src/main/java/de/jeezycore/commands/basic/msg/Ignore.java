3f45acb46952f9ced16de07b1ca712b3a0405a8a
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-07-11 18:36:59 +0200

    added new created methods to Ignore and UnIgnore classes and recoded it

--- JeezyCore/src/main/java/de/jeezycore/commands/basic/msg/Ignore.java
+++ JeezyCore/src/main/java/de/jeezycore/commands/basic/msg/Ignore.java
@@ -1,5 +1,6 @@
 package de.jeezycore.commands.basic.msg;
 
+import de.jeezycore.db.SettingsSQL;
 import de.jeezycore.utils.UUIDChecker;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,7 +10,7 @@ import org.bukkit.entity.Player;
 import static de.jeezycore.utils.ArrayStorage.msg_ignore_list;
 
 public class Ignore implements CommandExecutor {
-    UUIDChecker uuidChecker = new UUIDChecker();
+    SettingsSQL settingsSQL = new SettingsSQL();
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -17,15 +18,9 @@ public class Ignore implements CommandExecutor {
             Player p = (Player) sender;
 
             if (cmd.getName().equalsIgnoreCase("ignore") && args.length == 1) {
-                uuidChecker.check(args[0]);
-                if (msg_ignore_list.get(p.getPlayer().getUniqueId()) != null && msg_ignore_list.get(p.getPlayer().getUniqueId()).contains(UUIDChecker.uuid)) {
-                        p.sendMessage("§7You have already §cignored §7that player.");
-                        return true;
-                } else {
-                    String oldIgnores = msg_ignore_list.get(p.getPlayer().getUniqueId());
-                    msg_ignore_list.put(p.getPlayer().getUniqueId(), oldIgnores + UUIDChecker.uuid);
-                    p.sendMessage("§7You §2successfully §7ignored §9" + args[0] + "§7.");
-                }
+                    settingsSQL.addIgnore(p, args[0]);
+            } else {
+            settingsSQL.showIgnoreList(p);
             }
         }
         return true;
--- JeezyCore/src/main/java/de/jeezycore/commands/basic/msg/UnIgnore.java
+++ JeezyCore/src/main/java/de/jeezycore/commands/basic/msg/UnIgnore.java
@@ -1,15 +1,14 @@
 package de.jeezycore.commands.basic.msg;
 
-import de.jeezycore.utils.UUIDChecker;
+import de.jeezycore.db.SettingsSQL;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import static de.jeezycore.utils.ArrayStorage.msg_ignore_list;
 
 public class UnIgnore implements CommandExecutor {
-    UUIDChecker uuidChecker = new UUIDChecker();
+    SettingsSQL settingsSQL = new SettingsSQL();
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -17,18 +16,10 @@ public class UnIgnore implements CommandExecutor {
             Player p = (Player) sender;
 
             if (cmd.getName().equalsIgnoreCase("unignore") && args.length == 1) {
-                   uuidChecker.check(args[0]);
-                    if (msg_ignore_list.get(p.getPlayer().getUniqueId()) != null && msg_ignore_list.get(p.getPlayer().getUniqueId()).contains(UUIDChecker.uuid)) {
-                            String updatedIgnoreList = msg_ignore_list.get(p.getPlayer().getUniqueId()).replace(UUIDChecker.uuid, "");
-                            msg_ignore_list.put(p.getPlayer().getUniqueId(), updatedIgnoreList);
-                            p.sendMessage("§7You have §2successfully §7removed §9"+args[0]+" §7from the ignore list.");
-                            System.out.println(msg_ignore_list.get(p.getPlayer().getUniqueId()));
-                            return true;
-
+                  settingsSQL.removeIgnore(p, args[0]);
                     } else {
-                       p.sendMessage("§7You haven't §cignored §9"+args[0]+" §7yet.");
+                       p.sendMessage("Usage: /unignore <playerName>");
                     }
-            }
         }
         return true;
     }
