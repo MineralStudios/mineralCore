484814fe1feb461a81046c72a48a2f4bbdff0615
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-07-14 22:56:15 +0200

    added ChatColorSQL to get data also created ChatColorsInventory

--- JeezyCore/src/main/java/de/jeezycore/db/ChatColorSQL.java
+++ JeezyCore/src/main/java/de/jeezycore/db/ChatColorSQL.java
@@ -5,6 +5,8 @@ import org.bukkit.configuration.MemorySection;
 import org.bukkit.entity.Player;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChatColorSQL {
 
@@ -13,6 +15,16 @@ public class ChatColorSQL {
     String password;
 
     String chatColorName;
+
+    String colorName;
+    String color;
+    String colorRGB;
+
+    public static String currentChatColorName;
+
+    public static String currentChatColor;
+
+    public ArrayList<String> chatColorArray = new ArrayList<>();
 
 
     private void createConnection() {
@@ -53,6 +65,94 @@ public class ChatColorSQL {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void getChatColorsData() {
+        try {
+            this.createConnection();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+            String sql_select = "SELECT * FROM chatColors ORDER BY colorPriority DESC";
+            ResultSet rs = stm.executeQuery(sql_select);
+
+            while (rs.next()) {
+                colorName = rs.getString(1);
+                color = rs.getString(2);
+                colorRGB = rs.getString(3);
+
+                chatColorArray.add(Arrays.asList(colorName, color, colorRGB).toString());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getPlayerChatName(Player p) {
+        try {
+            this.createConnection();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+            String sql_select = "SELECT chatColor FROM players WHERE playerUUID = '"+p.getUniqueId()+"'";
+            ResultSet rs = stm.executeQuery(sql_select);
+            if (!rs.next()) {
+                currentChatColorName = null;
+            } else {
+                do {
+                    currentChatColorName = rs.getString(1);
+                } while (rs.next());
+            }
+            this.getPlayerChatColor(p);
+        } catch (SQLException f) {
+            f.printStackTrace();
+        }
+    }
+
+    public void getPlayerChatColor(Player p) {
+        try {
+            this.createConnection();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+            String sql_select = "SELECT * FROM chatColors WHERE colorName = '"+currentChatColorName+"'";
+            ResultSet rs = stm.executeQuery(sql_select);
+            if (!rs.next()) {
+                currentChatColor = null;
+            } else {
+                do {
+                    currentChatColor = rs.getString(2);
+                } while (rs.next());
+            }
+        } catch (SQLException f) {
+            f.printStackTrace();
+        }
+    }
+
+    public void Granting(org.bukkit.event.inventory.InventoryClickEvent e) {
+        try {
+            this.createConnection();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+
+                String updateChatColor = "UPDATE players " +
+                        "SET chatColor = '"+e.getCurrentItem().getItemMeta().getDisplayName().substring(2)+"'" +
+                        " WHERE playerUUID = '"+e.getWhoClicked().getUniqueId()+"'";
+                stm.executeUpdate(updateChatColor);
+        } catch (SQLException f) {
+            f.printStackTrace();
+        }
+    }
+
+    public void resetChatColor(org.bukkit.event.inventory.InventoryClickEvent e) {
+        try {
+            this.createConnection();
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+            String updateChatColor = "UPDATE players " +
+                    "SET chatColor = NULL" +
+                    " WHERE playerUUID = '"+e.getWhoClicked().getUniqueId()+"'";
+            stm.executeUpdate(updateChatColor);
+        } catch (SQLException f) {
+            f.printStackTrace();
         }
     }
 }
---
+++ JeezyCore/src/main/java/de/jeezycore/events/inventories/ChatColorsInventory.java
@@ -0,0 +1,131 @@
+package de.jeezycore.events.inventories;
+
+import de.jeezycore.db.ChatColorSQL;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatColorsInventory {
+
+    Inventory chatColorsInv;
+
+   int addUp = 10;
+
+    ChatColorSQL chatColorSQL = new ChatColorSQL();
+
+    public void run(org.bukkit.event.inventory.InventoryClickEvent e) {
+        if (e.getInventory().getTitle().contains("§9§lChat§f§lColors")) {
+            if (e.getCurrentItem().getData().toString().equalsIgnoreCase("LEATHER_CHESTPLATE(0)") && e.getCurrentItem().getItemMeta().getLore().get(5).equalsIgnoreCase("§a§lYou own this chatColor§7§l.")) {
+                if (e.getCurrentItem().getItemMeta().getDisplayName().substring(2).equalsIgnoreCase(ChatColorSQL.currentChatColorName)) {
+                    e.getWhoClicked().sendMessage("§7You have §c§lalready §7selected that chat color.");
+                    e.getWhoClicked().closeInventory();
+                    return;
+                }
+                e.getWhoClicked().sendMessage("§7You §2§lsuccessfully §7gave yourself the §9§l"+e.getCurrentItem().getItemMeta().getDisplayName()+ " §7chat color§7.");
+                e.getWhoClicked().closeInventory();
+                 executeMYSQL(e);
+            } else if (e.getCurrentItem().getData().toString().equalsIgnoreCase("NAME_TAG(0)") && e.getCurrentItem().getItemMeta().getLore().get(5).equalsIgnoreCase("§a§lYou own this chatColor§7§l.")) {
+                e.getWhoClicked().sendMessage("§a§lYou own this chatColor§7§l.");
+            }
+
+            if (e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§cReset chat color")) {
+                chatColorSQL.resetChatColor(e);
+                e.getWhoClicked().sendMessage("§7You §2successfully §creset §7your chatcolor.");
+                e.getWhoClicked().closeInventory();
+            }
+            e.setCancelled(true);
+        }
+    }
+
+    public void chatColorsMenu(Player p) {
+        chatColorSQL.chatColorArray.clear();
+        chatColorSQL.getChatColorsData();
+        chatColorSQL.getPlayerChatName(p);
+
+        chatColorsInv = Bukkit.createInventory(null, 45,"§9§lChat§f§lColors");
+
+        for (int i = 0; i < chatColorsInv.getSize(); i++) {
+            ItemStack placeholder = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) ((int) 11));
+            ItemMeta placeholderMeta = placeholder.getItemMeta();
+            placeholderMeta.setDisplayName("§9§lChoose your chatColor!");
+            placeholder.setItemMeta(placeholderMeta);
+            chatColorsInv.setItem(i, placeholder);
+        }
+
+            ItemStack resetChatColor = new ItemStack(Material.TORCH, 1);
+            ItemMeta resetChatColorMeta = resetChatColor.getItemMeta();
+            List<String> resetChatColorText = new ArrayList<>();
+            resetChatColorMeta.setDisplayName("§cReset chat color");
+            resetChatColorText.add(0, "§8§m-----------------------------------");
+            resetChatColorText.add(1, "§9§l"+ChatColorSQL.currentChatColorName);
+            resetChatColorText.add(2, "§7Current chat color display: §2"+ChatColorSQL.currentChatColor+p.getDisplayName()+"§7§l.");
+            resetChatColorText.add(3, "§8§m-----------------------------------");
+            resetChatColorText.add(4, "§eClick to reset your chatColor§7.");
+            resetChatColorMeta.setLore(resetChatColorText);
+            resetChatColor.setItemMeta(resetChatColorMeta);
+
+            if (ChatColorSQL.currentChatColorName != null) {
+                chatColorsInv.setItem(4, resetChatColor);
+            }
+
+            ItemStack customChatColorItem = new ItemStack(Material.BLAZE_ROD);
+            ItemMeta customChatColorMeta = customChatColorItem.getItemMeta();
+            List<String> customChatColorText = new ArrayList<>();
+            customChatColorMeta.setDisplayName("§9§lCustom §f§lchatColor");
+            customChatColorText.add(0, "§8§m-----------------------------------");
+            customChatColorText.add(1, "§9§lCreate your own custom chat color");
+            customChatColorText.add(2, "§8§m-----------------------------------");
+            customChatColorText.add(3, "§cLeft click to create your own.");
+            customChatColorMeta.setLore(customChatColorText);
+            customChatColorItem.setItemMeta(customChatColorMeta);
+            chatColorsInv.setItem(8, customChatColorItem);
+
+
+        for (int i = 0; i < chatColorSQL.chatColorArray.size(); i++) {
+            String access = chatColorSQL.chatColorArray.get(i).replace("[", "").replace(",", "").replace("]", "");
+            String colorName = access.split(" ")[0];
+            String color = access.split(" ")[1];
+            String red = access.split(" ")[2];
+            String green = access.split(" ")[3];
+            String blue = access.split(" ")[4];
+
+            ItemStack placeColors = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+            LeatherArmorMeta placeColorsMeta = (LeatherArmorMeta) placeColors.getItemMeta();
+            List<String> placeColorsDesc = new ArrayList<>();
+            placeColorsDesc.add(0, "§8§m-----------------------------------");
+            placeColorsDesc.add(1, "");
+            placeColorsDesc.add(2, "§7§lDisplay: §2" + color+p.getDisplayName());
+            placeColorsDesc.add(3, "");
+            placeColorsDesc.add(4, "§8§m-----------------------------------");
+            if (p.hasPermission("jeezy.core.chatColors.all")) {
+                placeColorsDesc.add(5, "§a§lYou own this chatColor§7§l.");
+            } else {
+                placeColorsDesc.add(5, "§4§lYou don't own this chatColor yet§7§l.");
+            }
+            if (i == 7 || i == 14) {
+                addUp += 2;
+            } else if (i >= 21) {
+                break;
+            }
+            placeColorsMeta.setColor((Color.fromRGB(Integer.parseInt(red), Integer.parseInt(green), Integer.parseInt(blue))));
+            placeColorsMeta.setDisplayName(color+colorName);
+            placeColorsMeta.setLore(placeColorsDesc);
+            placeColors.setItemMeta(placeColorsMeta);
+            chatColorsInv.setItem(addUp + i, placeColors);
+        }
+        p.openInventory(chatColorsInv);
+        addUp = 10;
+    }
+
+    private void executeMYSQL(org.bukkit.event.inventory.InventoryClickEvent e) {
+        chatColorSQL.Granting(e);
+    }
+}
