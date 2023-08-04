9d2a07a2696d0586c58046f8a5027df3dad47c7c
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-08-05 00:31:44 +0200

    added world delete command to world commands

--- JeezyCore/src/main/java/de/jeezycore/commands/basic/World.java
+++ JeezyCore/src/main/java/de/jeezycore/commands/basic/World.java
@@ -1,6 +1,7 @@
 package de.jeezycore.commands.basic;
 
 import de.jeezycore.config.JeezyConfig;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.WorldCreator;
@@ -9,18 +10,16 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.MemorySection;
 import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.File;
 import java.util.List;
 
 public class World implements CommandExecutor {
 
-    MemorySection mc = (MemorySection) JeezyConfig.multiverse_defaults.get("multiverse");
+    MemorySection multiverseWorlds = (MemorySection) JeezyConfig.multiverse_defaults.get("Multiverse");
+    List<String> worlds = (List<String>) multiverseWorlds.get("worlds");
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
 
         if (sender instanceof Player) {
             Player p = (Player) sender;
@@ -31,17 +30,36 @@ public class World implements CommandExecutor {
                         case "load":
                             if (args.length == 2) {
                                 new WorldCreator(args[1]).createWorld();
-                                p.sendMessage("§2Successfully §7loaded §f/ §7created the world §9"+ args[1]+"§7.");
 
+                                worlds.add(args[1]);
 
-                                /*
-                                Location worldObject = p.getLocation();
+                                JeezyConfig.multiverse_defaults.set("Multiverse.worlds", worlds);
 
-                                List<Location> locations = new ArrayList<>(Arrays.asList(worldObject));
-                                JeezyConfig.config_defaults.set("entry-spawn-point", locations);
-                                JeezyConfig.config_defaults.save(JeezyConfig.config);
-                                 */
+                                JeezyConfig.multiverse_defaults.save(JeezyConfig.multiverse);
 
+                                p.sendMessage("§7§l[§9§lWorlds§7] §2Successfully §7loaded §f/ §7created the world §9"+ args[1]+"§7.");
+                            } else {
+                                helpMessage(p);
+                            }
+                            break;
+                        case "delete":
+                            if (args.length == 2) {
+                                if (!Bukkit.getServer().getWorlds().toString().contains(args[1])) {
+                                    p.sendMessage("§7§l[§9§lWorlds§7] §7The world §cdoesn't §7exist.");
+                                } else {
+
+                                    worlds.remove(args[1]);
+
+                                    JeezyConfig.multiverse_defaults.set("Multiverse.worlds", worlds);
+
+                                    JeezyConfig.multiverse_defaults.save(JeezyConfig.multiverse);
+
+                                    Bukkit.unloadWorld(args[1], false);
+
+                                    FileUtils.deleteDirectory(new File(args[1]));
+
+                                    p.sendMessage("§7§l[§9§lWorlds§7] §7You §2successfully §7deleted the world: §9"+args[1]+"§7.");
+                                }
                             } else {
                                 helpMessage(p);
                             }
@@ -80,14 +98,17 @@ public class World implements CommandExecutor {
     }
 
     public void loadWorldsOnStartup() {
-        new WorldCreator("StrafeHub").createWorld();
+        for (String worldNames : worlds) {
+            new WorldCreator(worldNames).createWorld();
+        }
     }
 
     private void helpMessage(Player p) {
         p.sendMessage(new String[] {
                 " §9§lWorlds §f§lHelp",
                 "                                                                   ",
-                " §f/§9world §fload §7<world> §f- §7Loads a world.",
+                " §f/§9world §fload / creates §7<world> §f- §7Loads / Creates a world.",
+                " §f/§9world §fdelete §7<world> §f- §7Deletes a world that has been loaded.",
                 " §f/§9world §ftp §7<world> §f- §7Teleports player to another world.",
                 " §f/§9world §flist §f- §7Shows all worlds.",
                 "                                                                   "
