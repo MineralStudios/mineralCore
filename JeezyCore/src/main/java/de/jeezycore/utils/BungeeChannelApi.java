dfe7596c34004173122a7d461445f00117e76022
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-08-06 04:50:06 +0200

    added BungeeChannelApi

---
+++ JeezyCore/src/main/java/de/jeezycore/utils/BungeeChannelApi.java
@@ -0,0 +1,40 @@
+package de.jeezycore.utils;
+
+import de.jeezycore.db.RanksSQL;
+import de.jeezycore.db.StaffSQL;
+import de.jeezycore.main.Main;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class BungeeChannelApi {
+
+    StaffSQL staffSQL = new StaffSQL();
+
+    RanksSQL display = new RanksSQL();
+
+    io.github.leonardosnt.bungeechannelapi.BungeeChannelApi api = io.github.leonardosnt.bungeechannelapi.BungeeChannelApi.of(Main.getPlugin(Main.class));
+
+    public void sendStaffMessages(AsyncPlayerChatEvent e) {
+        api.getServer()
+                .whenComplete((server, errorServer) -> {
+        api.getPlayerList("ALL")
+                .whenComplete((result, error) -> {
+
+                    staffSQL.getStaff();
+                    display.getPlayerInformation(e.getPlayer());
+                    String sql = "SELECT * FROM ranks WHERE rankName = '"+display.rankNameInformation+"'";
+                    display.displayChatRank(sql);
+
+                    for (int i = 0; i < StaffSQL.staff.size(); i++) {
+                        String new_message = e.getMessage().replace("@", "").trim();
+                        try {
+                            if (!result.contains(StaffSQL.staff.get(i))) {
+                                continue;
+                            }
+                            api.sendMessage(StaffSQL.staff.get(i), "§7[§9Staff§7-§fChat§7] §7(§9"+server+"§7) "+display.rankColor.replace("&", "§")+e.getPlayer().getDisplayName()+"§f: "+new_message);
+                        } catch (Exception f) {
+                        }
+                    }
+                 });
+                });
+    }
+}
