e83bf89bc348c1f61fdfd743050b4ca180d3bbfa
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2024-07-20 05:08:16 +0200

    removed special nametag for bots

--- JeezyCore/src/main/java/de/jeezycore/utils/FakePlayerChecker.java
+++ JeezyCore/src/main/java/de/jeezycore/utils/FakePlayerChecker.java
@@ -6,12 +6,7 @@ import org.bukkit.entity.Player;
 
 public class FakePlayerChecker {
 
-   private static final NameTag nameTag = new NameTag();
-
     public static boolean isFakePlayer(Player player) {
-
-        nameTag.specialBotTag(player);
-
         return FakePlayerManager.getFakePlayer(player.getUniqueId()) != null;
     }
 }
