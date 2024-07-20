065de7463249a3c3d8db26894d6d3adfcdd8a7bb
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2024-07-20 02:02:20 +0200

    added custom motd support and added unlimited players when out of maintenance

--- JeezyCore/src/main/java/de/jeezycore/velocity/events/PingEventListener.java
+++ JeezyCore/src/main/java/de/jeezycore/velocity/events/PingEventListener.java
@@ -5,21 +5,35 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.proxy.server.ServerPing;
 import de.jeezycore.velocity.db.MaintenanceSQL;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+import static de.jeezycore.velocity.config.JeezyConfig.toml;
 
 public class PingEventListener {
-
 
 
     @Subscribe(order = PostOrder.FIRST)
     public void onProxyPing(final ProxyPingEvent event) {
 
+         ServerPing ping = event.getPing();
+         ServerPing.Builder builder = ping.asBuilder();
+
+        Component motDMaintenance = MiniMessage.miniMessage().deserialize(
+                toml.getString("motd_maintenance")
+        );
+
+        Component motD = MiniMessage.miniMessage().deserialize(
+                toml.getString("motd")
+        );
+
         if (MaintenanceSQL.maintenance) {
-            final ServerPing ping = event.getPing();
-            final ServerPing.Builder builder = ping.asBuilder();
-
-            builder.version(new ServerPing.Version(event.getConnection().getProtocolVersion().getProtocol()+1, "Whitelisted"));
-
-            event.setPing(builder.build());
+            builder.version(new ServerPing.Version(event.getConnection().getProtocolVersion().getProtocol() + 1, toml.getString("maintenance_display")));
+            builder.description(motDMaintenance);
+        } else {
+            builder.maximumPlayers(0);
+            builder.description(motD);
         }
+        event.setPing(builder.build());
     }
 }
