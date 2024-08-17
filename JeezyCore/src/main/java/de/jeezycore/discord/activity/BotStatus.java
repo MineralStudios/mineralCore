65dbf0d0aa045fdca3381df5f6735ee60a265983
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2024-08-18 01:11:59 +0200

    Set the bot status to dnd

--- JeezyCore/src/main/java/de/jeezycore/discord/activity/BotStatus.java
+++ JeezyCore/src/main/java/de/jeezycore/discord/activity/BotStatus.java
@@ -1,11 +1,13 @@
 package de.jeezycore.discord.activity;
 
+import org.javacord.api.entity.user.UserStatus;
+
 import static de.jeezycore.discord.JeezyBot.api;
 
 public class BotStatus {
 
     public void set () {
+        api.updateStatus(UserStatus.DO_NOT_DISTURB);
         api.updateActivity("► mineral.gg ◄");
     }
-
 }
