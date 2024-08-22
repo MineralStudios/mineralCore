acd5c34bd85ca295432bab196d562a44cdccca50
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2024-08-22 02:24:55 +0200

    redone IgnoreChat to check for perms

--- JeezyCore/src/main/java/de/jeezycore/events/chat/IgnoreChat.java
+++ JeezyCore/src/main/java/de/jeezycore/events/chat/IgnoreChat.java
@@ -10,15 +10,13 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import java.util.List;
 
 public class IgnoreChat implements Listener {
-    RanksSQL display = new RanksSQL();
     @EventHandler
     public void PlayerIgnoreChat(AsyncPlayerChatEvent e) {
         try {
             MemorySection mc = (MemorySection) JeezyConfig.config_defaults.get("chat");
             boolean chat_muted = mc.getBoolean("muted");
-            List<String> ignored_roles = (List<String>) mc.getList("ignored_roles_on_chat-mute");
 
-            if (!chat_muted || ignored_roles.contains(display.rank)) {
+            if (!chat_muted || e.getPlayer().hasPermission("server.chat.bypass")) {
                 System.out.println("Chat is enabled");
             } else {
                 System.out.println("Chat is disabled!");
